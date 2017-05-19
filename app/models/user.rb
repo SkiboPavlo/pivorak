df7f2738f97ce2554790d35adbd02baaeeb9c4fc
SkiboPavlo <skibo@i.ua>
2017-05-19 12:30:35 +0300

    fixed views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  rolify
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :omniauthable,
          :omniauth_providers => [:facebook, :google_oauth2, :twitter]
